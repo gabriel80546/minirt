1ad75917ff4c0287905f52ea592e144b99acbada
gabriel <gabriel80546@gmail.com>
2021-04-15 11:53:23 -0300

    simple scene

--- srcs/minirt.c
+++ srcs/minirt.c
@@ -54,57 +54,6 @@ t_vars	config_cams(t_vars input)
 	// 	i++;
 	// }
 
-	i = 0;
-	while (i < 6)
-	{
-		cam = (t_cam *)malloc(sizeof(t_cam));
-		cam->pos.x = 0.0;
-		cam->pos.y = 0.0;
-		cam->pos.z = -3.0;
-		cam->pos = rotacao_x(cam->pos, i * (PI / 16));
-		cam->pos = rotacao_y(cam->pos, i * (PI / 16));
-		cam->direc.x = -cam->pos.x;
-		cam->direc.y = -cam->pos.y;
-		cam->direc.z = -cam->pos.z;
-		cam->fov = 90.0;
-		list_add(saida.cams, cam);
-		i++;
-	}
-
-	i = 5;
-	while (i < (32 - 5))
-	{
-		cam = (t_cam *)malloc(sizeof(t_cam));
-		cam->pos.x = 0.0;
-		cam->pos.y = 0.0;
-		cam->pos.z = -3.0;
-		cam->pos = rotacao_x(cam->pos, 5 * (PI / 16));
-		cam->pos = rotacao_y(cam->pos, i * (PI / 16));
-		cam->direc.x = -cam->pos.x;
-		cam->direc.y = -cam->pos.y;
-		cam->direc.z = -cam->pos.z;
-		cam->fov = 90.0;
-		list_add(saida.cams, cam);
-		i++;
-	}
-
-	i = 27;
-	while (i < 32)
-	{
-		cam = (t_cam *)malloc(sizeof(t_cam));
-		cam->pos.x = 0.0;
-		cam->pos.y = 0.0;
-		cam->pos.z = -3.0;
-		cam->pos = rotacao_x(cam->pos, ((32 - i) * (PI / 16)));
-		cam->pos = rotacao_y(cam->pos, i * (PI / 16));
-		cam->direc.x = -cam->pos.x;
-		cam->direc.y = -cam->pos.y;
-		cam->direc.z = -cam->pos.z;
-		cam->fov = 90.0;
-		list_add(saida.cams, cam);
-		i++;
-	}
-
 	return (saida);
 }
 
@@ -114,12 +63,13 @@ t_vars	config_scene_easy(t_vars input)
 	t_objeto	*obj;
 	t_light		*light;
 	t_vec		temp_direc;
+	// int			i;
 
 	vars = input;
 	vars.largura = 400;
 	vars.altura = 300;
 	vars.gray = 0;
-	vars.ambient = 0x333333;
+	vars.ambient = 0x505050;
 	vars.objs = NULL;
 	obj = NULL;
 
@@ -138,7 +88,7 @@ t_vars	config_scene_easy(t_vars input)
 
 	obj = (t_objeto *)malloc(sizeof(t_objeto));
 	obj->tipo = PLANE;
-	obj->pl.pos.x = 10.0;
+	obj->pl.pos.x =  5.0;
 	obj->pl.pos.y =  0.0;
 	obj->pl.pos.z =  0.0;
 	obj->pl.direc.x = 1.0;
@@ -153,11 +103,35 @@ t_vars	config_scene_easy(t_vars input)
 	obj->tipo = PLANE;
 	obj->pl.pos.x =  0.0;
 	obj->pl.pos.y =  0.0;
-	obj->pl.pos.z = 10.0;
+	obj->pl.pos.z =  5.0;
 	obj->pl.direc.x = 0.0;
 	obj->pl.direc.y = 0.0;
 	obj->pl.direc.z = -1.0;
 	obj->pl.cor = 0xD040D0;
+	// vars.objs = list_init(obj);
+	list_add(vars.objs, obj);
+
+	obj = (t_objeto *)malloc(sizeof(t_objeto));
+	obj->tipo = PLANE;
+	obj->pl.pos.x =  -5.0;
+	obj->pl.pos.y =   0.0;
+	obj->pl.pos.z =   0.0;
+	obj->pl.direc.x = 1.0;
+	obj->pl.direc.y = 0.0;
+	obj->pl.direc.z = 0.0;
+	obj->pl.cor = 0x20FF40;
+	// vars.objs = list_init(obj);
+	list_add(vars.objs, obj);
+
+	obj = (t_objeto *)malloc(sizeof(t_objeto));
+	obj->tipo = PLANE;
+	obj->pl.pos.x =   0.0;
+	obj->pl.pos.y =   0.0;
+	obj->pl.pos.z =  -5.0;
+	obj->pl.direc.x = 0.0;
+	obj->pl.direc.y = 0.0;
+	obj->pl.direc.z = 1.0;
+	obj->pl.cor = 0x8040FF;
 	// vars.objs = list_init(obj);
 	list_add(vars.objs, obj);
 
@@ -200,11 +174,9 @@ t_vars	config_scene_easy(t_vars input)
 	obj->sp.cor = 0x8080FF;
 	list_add(vars.objs, obj);
 
-
-	temp_direc.x = 3.0;
-	temp_direc.y = 13.0;
-	temp_direc.z = 0.0;
-
+	temp_direc.x = -1.0;
+	temp_direc.y =  1.25;
+	temp_direc.z = -3.0;
 	// obj = (t_objeto *)malloc(sizeof(t_objeto));
 	// obj->tipo = SPHERE;
 	// obj->sp.pos.x = temp_direc.x - 1.2;
@@ -218,21 +190,21 @@ t_vars	config_scene_easy(t_vars input)
 
 	vars.lights = NULL;
 	light = (t_light *)malloc(sizeof(t_light));
-	light->bright = 0.6;
+	light->bright = 0.5;
 	light->pos.x = temp_direc.x;
 	light->pos.y = temp_direc.y;
 	light->pos.z = temp_direc.z;
 	light->cor = 0xFFFFFF;
 	vars.lights = list_init(light);
 
-	light = (t_light *)malloc(sizeof(t_light));
-	light->bright = 0.3;
-	light->pos.x = 0.0;
-	light->pos.y = 3.0;
-	light->pos.z = 3.0;
-	light->cor = 0xFFFFFF;
-	// vars.lights = list_init(light);
-	list_add(vars.lights, light);
+	// light = (t_light *)malloc(sizeof(t_light));
+	// light->bright = 0.3;
+	// light->pos.x = 0.0;
+	// light->pos.y = 3.0;
+	// light->pos.z = 3.0;
+	// light->cor = 0xFFFFFF;
+	// // vars.lights = list_init(light);
+	// list_add(vars.lights, light);
 
 	// light = (t_light *)malloc(sizeof(t_light));
 	// light->bright = 0.5;
@@ -242,7 +214,6 @@ t_vars	config_scene_easy(t_vars input)
 	// light->cor = 0xFFFFFF;
 	// // vars.lights = list_init(light);
 	// list_add(vars.lights, light);
-
 	return (vars);
 }
 
