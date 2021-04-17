88e618ed68eb72e5a8947a8696289a68e0a700a3
gabriel <gabriel80546@gmail.com>
2021-04-17 19:02:47 -0300

    added some functions

--- srcs/draw.c
+++ srcs/draw.c
@@ -1,8 +1,74 @@
 
 #include "minirt.h"
 
+double	absolute(double n)
+{
+	if (n < 0.0)
+		return -n;
+	else
+		return n;
+}
+
+int	equal(double a, double b)
+{
+	if (absolute(a - b) < EPSILON)
+		return (1);
+	else
+		return (0);
+}
+
+t_tuple	mul_scalar(t_tuple input, double scalar)
+{
+	t_tuple	saida;
+
+	saida = input;
+	saida.x *= scalar;
+	saida.y *= scalar;
+	saida.z *= scalar;
+	saida.w *= scalar;
+	return (saida);
+}
+
+double	magnitude(t_tuple A)
+{
+	double a;
+	double b;
+	double c;
+	double d;
+
+	a = (A.x * A.x);
+	b = (A.y * A.y);
+	c = (A.z * A.z);
+	d = (A.w * A.w);
+	d = sqrt((a * a) + (b * b) + (c * c) + (d * d));
+	return (d);
+}
+
+
+void	print_tuple(t_tuple tupla)
+{
+	printf("tupla.x = % 6.6lf\n", tupla.x);
+	printf("tupla.y = % 6.6lf\n", tupla.y);
+	printf("tupla.z = % 6.6lf\n", tupla.z);
+	printf("tupla.w = % 6.6lf\n", tupla.w);
+}
+
 void	draw_main(t_vars vars, int x, int y, t_img img)
 {
+	t_tuple	teste;
+
+	teste.x =  1;
+	teste.y = -2;
+	teste.z =  3;
+	teste.w = -4;
+	if (x == 3 && y == 5)
+	{
+		print_tuple(teste);
+		printf("\n");
+		print_tuple(mul_scalar(teste, 3.5));
+		printf("\n");
+		print_tuple(mul_scalar(teste, 0.5));
+	}
 	if (x + y > (vars.largura / 2))
 		*((unsigned int *)img.data + (x + (y * img.size_line / (img.bits_per_pixel / img.bits_per_byte)))) = 0xFF0000;
 	else
--- srcs/minirt.h
+++ srcs/minirt.h
@@ -14,35 +14,19 @@
 # define PLANE 2
 # define CYLINDER 3
 
-# define EPSILON 0.0000305176
-# define EPSILON_ZERO 0.00000000305176
+# define EPSILON 0.00001
 
 # define PI 3.1415926535897932384626433832795028841971
 
-typedef struct	s_posicao {
-	double		x;
-	double		y;
-	double		z;
-}				t_posicao;
 
-typedef struct	s_coeff
+typedef struct	s_tuple
 {
-	double		a;
-	double		b;
-	double		c;
-}				t_coeff;
+	double	x;
+	double	y;
+	double	z;
+	double	w;
+}				t_tuple;
 
-typedef struct	s_reta
-{
-	t_vec		orig;
-	t_vec		dest;
-}				t_reta;
-
-typedef struct	s_reta_or_n
-{
-	t_reta		r;
-	int			n;
-}				t_reta_or_n;
 
 
 typedef struct	s_esfera
@@ -108,15 +92,7 @@ typedef struct  s_vars {
 	t_list		*cams;
 	int			ambient;
 	int			gray;
-	// t_light		light;
 }               t_vars;
-
-typedef struct	s_cor_had
-{
-	double		r;
-	double		g;
-	double		b;
-}				t_cor_had;
 
 typedef struct s_img
 {
@@ -143,37 +119,6 @@ typedef struct	s_debug
 }				t_debug;
 
 
-t_reta		empty_reta(void);
-t_vec		empty_vec(void);
-t_coeff		get_sp_coeff(t_vec A, t_vec B, t_esfera sp/* , int print_flag */);
-t_list		*cruzamento_sp_reta(t_vec A, t_vec B, t_esfera sp);
-// t_list		*sanitize_cruz(t_vec cam, t_vec tela, t_list *result);
-t_list		*sanitize_cruz(t_vec cam, t_vec tela, t_list *result);
-t_list		*sanitize_cruz_two(t_vec cam, t_vec tela, t_list *result);
-double		cruzamento_sp_delta(t_vec A, t_vec B, t_esfera sp);
-int			cruza_sp(t_vec A, t_vec B, t_esfera sp);
-
-t_hit		closest_hit(t_list *hits, t_vec pos);
-t_list		*get_all_hits(t_vars vars, t_vec tela);
-
-int			can_light_see_this_hit(t_hit hit, t_vars vars, t_light light);
-int			compute_color(double intensity, int input);
-
 void		draw(t_vars vars);
-void		draw_yellow_sp(t_vars vars);
-void		draw_indiano(/* t_vars vars */);
-void		clear_screen(t_vars vars);
-
-int			to_rgb(t_cor_had cor);
-t_cor_had	to_had(int rgb);
-t_cor_had	norm_had(t_cor_had input);
-t_cor_had	mult_had(t_cor_had a, t_cor_had b);
-t_cor_had	sum_had(t_cor_had a, t_cor_had b);
-
-void		test_image(t_vars vars);
-
-
-t_list		*cruzamento_pl_reta(t_vec A, t_vec B, t_plano pl);
-t_list		*cruzamento_cy_reta(t_vec A, t_vec B, t_cylinder cy);
 
 #endif
