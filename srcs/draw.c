2d51771c0c257da3dfd2b55c3abba050f9ac0f8b
Gabriel <gabriel80546@gmail.com>
2021-04-21 14:19:48 -0300

    added is_shadowed

--- srcs/draw.c
+++ srcs/draw.c
@@ -703,6 +703,46 @@ t_cor	lighting(t_material material, t_light light, t_tuple point, t_tuple eyev, 
 	return (color_add(color_add(ambient, diffuse), specular));
 }
 
+t_cor	lighting_new(t_material material, t_light light, t_tuple point, t_tuple eyev, t_tuple normalv, int in_shadow)
+{
+	t_cor	effective_color;
+	t_cor	ambient;
+	t_tuple lightv;
+	t_tuple	reflectv;
+	double	reflect_dot_eye;
+	double	light_dot_normal;
+	t_cor	diffuse;
+	t_cor	specular;
+	double	factor;
+
+	effective_color = color_mul(material.color, light.cor);
+	lightv = normalize(tup_sub(light.position, point));
+	ambient = color_mul_scalar(effective_color, material.ambient);
+	light_dot_normal = dot(lightv, normalv);
+	if (light_dot_normal < 0.0)
+	{
+		diffuse = color(0.0, 0.0, 0.0);
+		specular = color(0.0, 0.0, 0.0);
+	}
+	else
+	{
+		diffuse = color_mul_scalar(effective_color, material.diffuse * light_dot_normal);
+		reflectv = reflect(mul_scalar(lightv, -1.0), normalv);
+		reflect_dot_eye = dot(reflectv, eyev);
+		if (reflect_dot_eye <= 0)
+			specular = color(0.0, 0.0, 0.0);
+		else
+		{
+			factor = pow(reflect_dot_eye, material.shininess);
+			specular = color_mul_scalar(light.cor, material.specular * factor);
+		}
+	}
+	if (in_shadow == 1)
+		return (ambient);
+	else
+		return (color_add(color_add(ambient, diffuse), specular));
+}
+
 void	put_pixel(t_img *img, int x, int y, t_cor cor)
 {
 	int ratio;
@@ -908,11 +948,39 @@ t_ray	ray_for_pixel(t_camera camera, int px, int py)
 	return (ray);
 }
 
+
+int	is_shadowed(t_vars world, t_tuple point)
+{
+	t_tuple	v;
+	double	dist;
+	t_tuple	direction;
+	t_ray	r;
+	t_list	*hits;
+	t_hit	hit;
+
+	v = tup_sub(((t_light *)world.lights->data)->position, point);
+	dist = magnitude(v);
+	direction = normalize(v);
+	r = ray_create(point, direction);
+	hits = intersect_world(world, r);
+	hit = closest_hit(hits);
+	if (hit.t != -42.0 && hit.t < dist)
+		return (1);
+	else
+		return (0);
+}
+
+
 void	draw_main(t_vars vars, int x, int y, t_img img)
 {
 	t_ray	ray;
 	t_cor	hit_cor;
 	t_camera	camera;
+	t_material	m;
+	t_tuple		eyev;
+	t_tuple		normalv;
+	t_light		light;
+	int			in_shadow;
 
 	camera = setup_camera(vars);
 	ray = ray_for_pixel(camera, x, y);
@@ -921,7 +989,64 @@ void	draw_main(t_vars vars, int x, int y, t_img img)
 	
 	if (x == 3 && y == 5)
 	{
-		// testes
+		// Scenario: Lighting with the surface in shadow
+		// Given eyev ← vector(0, 0, -1)
+		// And normalv ← vector(0, 0, -1)
+		// And light ← point_light(point(0, 0, -10), color(1, 1, 1))
+		// And in_shadow ← true
+		// When result ← lighting(m, light, position, eyev, normalv, in_shadow)
+		// Then result = color(0.1, 0.1, 0.1)
+		// say("Hello World!\n", DEB);
+
+		eyev = vector(0, 0, -1);
+		normalv = vector(0, 0, -1);
+		light.position = point(0, 0, -10);
+		light.cor = color(1, 1, 1);
+		in_shadow = 1;
+
+		m.color = color(1.0, 1.0, 1.0);
+		m.ambient = 0.1;
+		m.diffuse = 0.7;
+		m.specular = 0.2;
+		m.shininess = 200.0;
+
+		hit_cor = lighting_new(m, light, point(0, 0, 0), eyev, normalv, in_shadow);
+		// say("hit_cor.r = % 6.6lf\n", DEB, hit_cor.r);
+		// say("hit_cor.g = % 6.6lf\n", DEB, hit_cor.g);
+		// say("hit_cor.b = % 6.6lf\n", DEB, hit_cor.b);
+		// When result ← lighting(m, light, position, eyev, normalv, in_shadow)
+		// Then result = color(0.1, 0.1, 0.1)
+	
+
+		say("****************Test****************\n", DEB);
+		say("Scenario: There is no shadow when nothing is collinear with point and light\n", DEB);
+		say("Given w ← default_world()\n", DEB);
+		say("And p ← point(0, 10, 0)\n", DEB);
+		say("Then is_shadowed(w, p) is false\n", DEB);
+		say("is_shadowed(vars, point(0, 10, 0)) = %d\n", DEB, is_shadowed(vars, point(0, 10, 0)));
+
+		say("****************Test****************\n", DEB);
+		say("Scenario: The shadow when an object is between the point and the light\n", DEB);
+		say("Given w ← default_world()\n", DEB);
+		say("And p ← point(10, -10, 10)\n", DEB);
+		say("Then is_shadowed(w, p) is true\n", DEB);
+		say("is_shadowed(vars, point(10, -10, 10)) = %d\n", DEB, is_shadowed(vars, point(10, -10, 10)));
+
+		say("****************Test****************\n", DEB);
+		say("Scenario: There is no shadow when an object is behind the light\n", DEB);
+		say("Given w ← default_world()\n", DEB);
+		say("And p ← point(-20, 20, -20)\n", DEB);
+		say("Then is_shadowed(w, p) is false\n", DEB);
+		say("is_shadowed(vars, point(-20, 20, -20)) = %d\n", DEB, is_shadowed(vars, point(-20, 20, -20)));
+
+		say("****************Test****************\n", DEB);
+		say("Scenario: There is no shadow when an object is behind the point\n", DEB);
+		say("Given w ← default_world()\n", DEB);
+		say("And p ← point(-2, 2, -2)\n", DEB);
+		say("Then is_shadowed(w, p) is false\n", DEB);
+		say("is_shadowed(vars, point(-2, 2, -2)) = %d\n", DEB, is_shadowed(vars, point(-2, 2, -2)));
+
+
 	}
 }
 
--- srcs/minirt.c
+++ srcs/minirt.c
@@ -4,13 +4,15 @@
 
 int		say(const char *format, char *file, const char *func, int line, ...)
 {
-	va_list	args;
-	int		saida;
+	va_list		args;
+	int			saida;
+	static int	counter = 0;
 
-	printf("%s(%s:%d): ", file, func, line);
+	printf("%05d:%s(%s:%d): ", counter, file, func, line);
 	va_start(args, line);
 	saida = vprintf(format, args);
 	va_end(args);
+	counter++;
 	return (saida);
 }
 
@@ -32,28 +34,26 @@ t_vars	config_cams(t_vars input)
 	cam->direc.y = 0.0;
 	cam->direc.z = 3.0;
 	cam->fov = 60.0;
-	// cam->transform = mat44_mul(mat44_scaling(-1, 1, 1), mat44_translate(cam->pos.x, cam->pos.y, cam->pos.z));
 	cam->transform = view_transform(point(0, 1.5, -5), point(0, 1, 0), vector(0, 1, 0));
 	saida.cams = list_init(cam);
 	// list_add(saida.cams, cam);
 
-	i = 0;
-	while (i < 60)
-	{
-		cam = (t_cam *)malloc(sizeof(t_cam));
-		cam->pos.x = 0.0;
-		cam->pos.y = 0.0;
-		cam->pos.z = 5.0 - (double)(i * 0.25);
-		// cam->pos = rotacao_y(cam->pos, i * (PI / 16));
-		cam->direc.x = -cam->pos.x;
-		cam->direc.y = -cam->pos.y;
-		cam->direc.z = -cam->pos.z;
-		cam->fov = 60.0;
-		// cam->transform = mat44_mul(mat44_scaling(-1, 1, 1), mat44_translate(cam->pos.x, cam->pos.y, cam->pos.z));
-		cam->transform = view_transform(point(0, 1.5, -15 + (double)(i * 0.25)), point(0, 1, 0), vector(0, 1, 0));
-		list_add(saida.cams, cam);
-		i++;
-	}
+	// i = 0;
+	// while (i < 60)
+	// {
+	// 	cam = (t_cam *)malloc(sizeof(t_cam));
+	// 	cam->pos.x = 0.0;
+	// 	cam->pos.y = 0.0;
+	// 	cam->pos.z = 5.0 - (double)(i * 0.25);
+	// 	cam->direc.x = -cam->pos.x;
+	// 	cam->direc.y = -cam->pos.y;
+	// 	cam->direc.z = -cam->pos.z;
+	// 	cam->fov = 60.0;
+	// 	// cam->transform = mat44_mul(mat44_scaling(-1, 1, 1), mat44_translate(cam->pos.x, cam->pos.y, cam->pos.z));
+	// 	cam->transform = view_transform(point(0, 1.5, -5 + (double)(i * 0.25)), point(0, 1, 0), vector(0, 1, 0));
+	// 	list_add(saida.cams, cam);
+	// 	i++;
+	// }
 	i = 0;
 	while (i < 32)
 	{
@@ -66,7 +66,7 @@ t_vars	config_cams(t_vars input)
 		cam->direc.y = -cam->pos.y;
 		cam->direc.z = -cam->pos.z;
 		cam->fov = 60.0;
-		cam->transform = mat44_mul(mat44_scaling(-1, 1, 1), mat44_translate(cam->pos.x, cam->pos.y, cam->pos.z));
+		cam->transform = view_transform(mat44_tuple_mul(mat44_rotate_y(i * (PI / 16)), point(0, 1.5, -5)), point(0, 1, 0), vector(0, 1, 0));
 		list_add(saida.cams, cam);
 		i++;
 	}
@@ -84,7 +84,7 @@ t_vars	config_cams(t_vars input)
 		cam->direc.y = -cam->pos.y;
 		cam->direc.z = -cam->pos.z;
 		cam->fov = 60.0;
-		cam->transform = mat44_mul(mat44_scaling(-1, 1, 1), mat44_translate(cam->pos.x, cam->pos.y, cam->pos.z));
+		cam->transform = view_transform(point(0, 1.5, -5), point(0, 1, 0), vector(0, 1, 0));
 		list_add(saida.cams, cam);
 		i++;
 	}
@@ -102,7 +102,7 @@ t_vars	config_cams(t_vars input)
 		cam->direc.y = -cam->pos.y;
 		cam->direc.z = -cam->pos.z;
 		cam->fov = 60.0;
-		cam->transform = mat44_mul(mat44_scaling(-1, 1, 1), mat44_translate(cam->pos.x, cam->pos.y, cam->pos.z));
+		cam->transform = view_transform(point(0, 1.5, -5), point(0, 1, 0), vector(0, 1, 0));
 		list_add(saida.cams, cam);
 		i++;
 	}
@@ -120,7 +120,7 @@ t_vars	config_cams(t_vars input)
 		cam->direc.y = -cam->pos.y;
 		cam->direc.z = -cam->pos.z;
 		cam->fov = 60.0;
-		cam->transform = mat44_mul(mat44_scaling(-1, 1, 1), mat44_translate(cam->pos.x, cam->pos.y, cam->pos.z));
+		cam->transform = view_transform(point(0, 1.5, -5), point(0, 1, 0), vector(0, 1, 0));
 		list_add(saida.cams, cam);
 		i++;
 	}
@@ -180,6 +180,21 @@ t_vars	config_scene_easy(t_vars input)
 
 	// ESFERAS
 
+
+	obj = (t_objeto *)malloc(sizeof(t_objeto));
+	obj->tipo = SPHERE;
+	obj->sp.pos = point(0.0, 0.0, 0.0);
+	obj->sp.diametro = 157.0;
+	obj->sp.material.color = color(1.0, 0.0, 0.0);
+	obj->sp.material.ambient = 0.1;
+	obj->sp.material.diffuse = 0.7;
+	obj->sp.material.specular = 0.2;
+	obj->sp.material.shininess = 200.0;
+	obj->sp.transform = mat44_identity();
+	vars.objs = list_init(obj);
+	// list_add(vars.objs, obj);
+
+/*
 	obj = (t_objeto *)malloc(sizeof(t_objeto));
 	obj->tipo = SPHERE;
 	obj->sp.pos = point(0.0, 0.0, 0.0);
@@ -281,6 +296,7 @@ t_vars	config_scene_easy(t_vars input)
 							mat44_scaling(0.33, 0.33, 0.33));
 	// vars.objs = list_init(obj);
 	list_add(vars.objs, obj);
+*/
 
 
 
